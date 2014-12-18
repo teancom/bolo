fbbf5c370a037b0a5c8de313f760293b8a5af22d
James Hunt <james@niftylogic.com>
2014-12-18 17:07:53 -0500

    Configuration File parsing

--- src/config.c
+++ src/config.c
@@ -6,25 +6,27 @@
 
 #define LINE_BUF_SIZE 8192
 
-#define T_KEYWORD_LISTENER   1
-#define T_KEYWORD_CONTROL    2
-#define T_KEYWORD_NSCA_PORT  3
-#define T_KEYWORD_LOG        4
-#define T_KEYWORD_SAVEFILE   5
-#define T_KEYWORD_DUMPFILES  6
-#define T_KEYWORD_TYPE       7
-#define T_KEYWORD_FRESHNESS  8
-#define T_KEYWORD_STATE      9
-#define T_OPEN_BRACE        10
-#define T_CLOSE_BRACE       11
-#define T_STRING            12
-#define T_QSTRING           13
-#define T_NUMBER            14
-#define T_TYPENAME          15
+#define T_KEYWORD_LISTENER   0x01
+#define T_KEYWORD_CONTROL    0x02
+#define T_KEYWORD_NSCA_PORT  0x03
+#define T_KEYWORD_LOG        0x04
+#define T_KEYWORD_SAVEFILE   0x05
+#define T_KEYWORD_DUMPFILES  0x06
+#define T_KEYWORD_TYPE       0x07
+#define T_KEYWORD_FRESHNESS  0x08
+#define T_KEYWORD_STATE      0x09
+#define T_KEYWORD_USE        0x0a
+
+#define T_OPEN_BRACE         0x80
+#define T_CLOSE_BRACE        0x81
+#define T_STRING             0x82
+#define T_NUMBER             0x83
+#define T_TYPENAME           0x845
 
 typedef struct {
 	FILE *io;
-	char *token;
+	int   token;
+	char  tval[LINE_BUF_SIZE];
 	char  line[LINE_BUF_SIZE];
 	char  raw[LINE_BUF_SIZE];
 } parser_t;
@@ -33,28 +35,116 @@ static int lex(parser_t *p)
 {
 	char *a, *b;
 
+	if (!*p->line) {
 getline:
-	if (!fgets(p->raw, LINE_BUF_SIZE, p->io))
-		return 0;
+		if (!fgets(p->raw, LINE_BUF_SIZE, p->io))
+			return 0;
 
-	a = p->raw;
-	while (*a && isspace(*a)) a++;
-	if (*a == '#')
-		while (*a && *a != '\n') a++;
-	while (*a && isspace(*a)) a++;
-	if (!*a)
-		goto getline;
+		a = p->raw;
+		while (*a && isspace(*a)) a++;
+		if (*a == '#')
+			while (*a && *a != '\n') a++;
+		while (*a && isspace(*a)) a++;
+		if (!*a)
+			goto getline;
+
+		b = p->line;
+		while ((*b++ = *a++));
+	}
+	p->token = 0;
+	p->tval[0] = '\0';
+
+#define KEYWORD(s,k) if (strcmp(a, s) == 0) p->token = T_KEYWORD_ ## k
 
 	b = p->line;
-	while (*a && *a != '\n')
-		*b++ = *a++;
-	*b = '\0';
+	while (*b && isspace(*b)) b++;
+	a = b;
 
-	p->token = p->line;
-	return 1;
+	/* single-char sigils */
+	if (*b == '{') {
+		b++;
+		p->token = T_OPEN_BRACE;
+
+		while (*b && isspace(*b)) b++;
+		memmove(p->line, b, strlen(b)+1);
+		return 1;
+	}
+	if (*b == '}') {
+		b++;
+		p->token = T_CLOSE_BRACE;
+
+		while (*b && isspace(*b)) b++;
+		memmove(p->line, b, strlen(b)+1);
+		return 1;
+	}
+
+	if (isalnum(*b)) {
+		b++;
+		while (*b && (isalnum(*b) || ispunct(*b)))
+			b++;
+		if (!*b || isspace(*b)) {
+			*b++ = '\0';
+			KEYWORD("listener",  LISTENER);
+			KEYWORD("control",   CONTROL);
+			KEYWORD("nsca.port", NSCA_PORT);
+			KEYWORD("log",       LOG);
+			KEYWORD("savefile",  SAVEFILE);
+			KEYWORD("dumpfiles", DUMPFILES);
+			KEYWORD("type",      TYPE);
+			KEYWORD("freshness", FRESHNESS);
+			KEYWORD("state",     STATE);
+			KEYWORD("use",       USE);
+
+			if (!p->token) {
+				memcpy(p->tval, p->line, b-p->line);
+				p->token = T_STRING;
+			}
+
+			while (*b && isspace(*b)) b++;
+			memmove(p->line, b, strlen(b)+1);
+			return 1;
+		}
+		fprintf(stderr, "WARNING: next character ASCII %#02x unrecognized!\n", *b);
+		b = a;
+	}
+
+	if (*b == ':') {
+		b++;
+		while (*b && (isalnum(*b) || *b == '-' || *b == '_'))
+			b++;
+
+		if (!*b || isspace(*b)) {
+			*b++ = '\0';
+			memcpy(p->tval, p->line, b-p->line);
+			p->token = T_TYPENAME;
+
+			while (*b && isspace(*b)) b++;
+			memmove(p->line, b, strlen(b)+1);
+			return 1;
+		}
+		b = a;
+	}
+
+	if (*b == '"') {
+		b++; a = p->tval;
+		while (*b && *b != '"' && *b != '\r' && *b != '\n') {
+			if (*b == '\\') b++;
+			*a++ = *b++;
+		}
+		*a = '\0';
+		if (*b == '"') b++;
+		else fprintf(stderr, "WARNING: unterminated string literal\n");
+
+		p->token = T_STRING;
+		while (*b && isspace(*b)) b++;
+		memmove(p->line, b, strlen(b)+1);
+		return 1;
+	}
+
+	return 0;
 }
 
-int configure(const char *path, server_t *s, db_t *d)
+int configure(const char *path, server_t *s, db_t *db)
 {
 	parser_t p;
 	memset(&p, 0, sizeof(p));
@@ -62,10 +152,124 @@ int configure(const char *path, server_t *s, db_t *d)
 	p.io = fopen(path, "r");
 	if (!p.io) return -1;
 
-	while (lex(&p)) {
-		fprintf(stderr, "got a token: [%s]\n", p.token);
+#define NEXT if (!lex(&p)) goto bail
+#define SERVER_STRING(x) NEXT; if (p.token != T_STRING) goto esyntax; \
+	free(x); x = strdup(p.tval)
+#define SERVER_NUMBER(x) NEXT; if (p.token != T_STRING) goto esyntax; \
+	x = atoi(p.tval)
+
+	const char *default_type = NULL;
+	type_t *type = NULL;
+	state_t *state = NULL;
+
+	for (;;) {
+		if (!lex(&p)) break;
+
+		switch (p.token) {
+		case T_KEYWORD_LISTENER:  SERVER_STRING(s->config.bolo_endpoint); break;
+		case T_KEYWORD_CONTROL:   SERVER_STRING(s->config.stat_endpoint); break;
+		case T_KEYWORD_NSCA_PORT: SERVER_NUMBER(s->config.nsca_port);     break;
+		case T_KEYWORD_SAVEFILE:  SERVER_STRING(s->config.savefile);      break;
+		case T_KEYWORD_DUMPFILES: SERVER_STRING(s->config.dumpfiles);     break;
+
+		case T_KEYWORD_LOG:
+			SERVER_STRING(s->config.log_level);
+			SERVER_STRING(s->config.log_facility);
+			break;
+
+		case T_KEYWORD_USE:
+			NEXT; if (p.token != T_TYPENAME) goto esyntax;
+			default_type = strdup(p.tval);
+			break;
+
+		case T_KEYWORD_TYPE:
+			NEXT; if (p.token != T_TYPENAME) goto esyntax;
+			type = calloc(1, sizeof(type_t));
+			type->freshness = 300;
+			type->status    = WARNING;
+			hash_set(&db->types, p.tval, type);
+
+			NEXT; if (p.token != T_OPEN_BRACE) goto esyntax;
+			for (;;) {
+				NEXT; if (p.token == T_CLOSE_BRACE) break;
+				switch (p.token) {
+					case T_KEYWORD_FRESHNESS:
+						NEXT; if (p.token != T_STRING) goto esyntax;
+						type->freshness = atoi(p.tval);
+						break;
+
+					case T_STRING:
+						     if (strcmp(p.tval, "warning")  == 0) type->status = WARNING;
+						else if (strcmp(p.tval, "critical") == 0) type->status = CRITICAL;
+						else if (strcmp(p.tval, "unknown")  == 0) type->status = UNKNOWN;
+						else goto esyntax;
+						NEXT; if (p.token != T_STRING) goto esyntax;
+						type->summary = strdup(p.tval);
+						break;
+
+					default: goto esyntax;
+				}
+			}
+
+			if (!type->summary) {
+				if (type->freshness >= 3600) {
+					type->summary = string("No results received for more than %i hour%s",
+							type->freshness / 3600, type->freshness < 7200 ? "" : "s");
+				} else if (type->freshness >= 60) {
+					type->summary = string("No results received for more than %i minute%s",
+							type->freshness / 60, type->freshness < 120 ? "" : "s");
+				} else {
+					type->summary = string("No results received for more than %i second%s",
+							type->freshness, type->freshness == 1 ? "" : "s");
+				}
+			}
+			break;
+
+		case T_KEYWORD_STATE:
+			NEXT;
+			type = NULL;
+			if (p.token == T_TYPENAME) {
+				type = hash_get(&db->types, p.tval);
+				NEXT;
+
+			} else if (default_type) {
+				type = hash_get(&db->types, default_type);
+
+			}
+
+			if (p.token != T_STRING) goto esyntax;
+
+			if (!type) {
+				fprintf(stderr, "ERROR: failed to determine type for %s\n", p.tval);
+				goto bail;
+			}
+
+			state = calloc(1, sizeof(state_t));
+			state->name = strdup(p.tval);
+			hash_set(&db->states, p.tval, state);
+
+			state->name   = strdup(p.tval);
+			state->type   = type;
+			state->status = PENDING;
+			state->expiry = type->freshness + time_s();
+
+			break;
+
+		default:
+			goto esyntax;
+		}
 	}
+
+	if (!feof(p.io))
+		goto bail;
 
 	fclose(p.io);
 	return 0;
+
+esyntax:
+	fprintf(stderr, "syntax error\n");
+bail:
+	fprintf(stderr, "configuration failed\n");
+	fclose(p.io);
+	return 1;
 }
--- t/05-config.c
+++ t/05-config.c
@@ -2,11 +2,8 @@
 
 TESTS {
 	subtest { /* single-file */
-		db_t db;
-		memset(&db, 0, sizeof(db));
-
-		server_t s;
-		memset(&s, 0, sizeof(s));
+		db_t     db;  memset(&db,  0, sizeof(db));
+		server_t svr; memset(&svr, 0, sizeof(svr));
 
 		write_file("t/tmp/config",
 			"# test bolo configuration\n"
@@ -23,7 +20,7 @@ TESTS {
 			"savefile  t/tmp/save\n"
 			"dumpfiles t/tmp/dump.\%s\n"
 			"\n"
-			"type :local {\n"
+			"type :test {\n"
 			"  freshness 300\n"
 			"  critical \"local check stale!\"\n"
 			"}\n"
@@ -32,37 +29,156 @@ TESTS {
 			"  warning \"no result in over 15min\"\n"
 			"}\n"
 			"\n"
-			"use :local\n"
+			"type :unused {\n"
+			"  freshness 5\n"
+			"  unknown whatever\n"
+			"}\n"
+			"\n"
+			"use :test\n"
 			"state host01/cpu\n"
 			"state host02/cpu\n"
 			"state :cloud site/online\n"
 			"state host01/memory\n"
 			"", 0);
 
-		ok(configure("t/tmp/config", &s, &db) == 0,
+		int32_t now = time_s();
+		ok(configure("t/tmp/config", &svr, &db) == 0,
 			"Read configuration from t/tmp/config");
 
-		is(s.config.bolo_endpoint, "tcp://*:4444",   "listener set");
-		is(s.config.stat_endpoint, "tcp://*:5555",   "control set");
-		is_int(s.config.nsca_port,  5668,            "nsca.port set");
-		is(s.config.log_level,     "critical",       "log level set");
-		is(s.config.log_facility,  "daemon",         "log facility set");
-		is(s.config.savefile,      "t/tmp/save",     "savefile set");
-		is(s.config.dumpfiles,     "t/tmp/dump.\%s", "dumpfiles set");
+		is(svr.config.bolo_endpoint, "tcp://*:4444",   "listener set");
+		is(svr.config.stat_endpoint, "tcp://*:5555",   "control set");
+		is_int(svr.config.nsca_port,  5668,            "nsca.port set");
+		is(svr.config.log_level,     "critical",       "log level set");
+		is(svr.config.log_facility,  "daemon",         "log facility set");
+		is(svr.config.savefile,      "t/tmp/save",     "savefile set");
+		is(svr.config.dumpfiles,     "t/tmp/dump.\%s", "dumpfiles set");
 
 		type_t *t;
-		t = hash_get(&db.types, ":local");
-		isnt_null(t, "type :local created");
-		if (t) {
-		is(t->status, CRITICAL, ":local checks go CRITICAL when stale");
-		is(t->summary, "local check stale!", ":local checks stale summary");
-		}
+		t = hash_get(&db.types, ":test");
+		isnt_null(t, "type :test created");
+		is_int(t->status, CRITICAL, ":test checks go CRITICAL when stale");
+		is(t->summary, "local check stale!", ":test checks stale summary");
 
 		t = hash_get(&db.types, ":cloud");
 		isnt_null(t, "type :cloud created");
-		if (t) {
-		is(t->status, WARNING, ":cloud checks go WARNING when stale");
+		is_int(t->status, WARNING, ":cloud checks go WARNING when stale");
 		is(t->summary, "no result in over 15min", ":cloud checks stale summary");
-		}
+
+		t = hash_get(&db.types, ":enoent");
+		is_null(t, "type :enoent not created (not in the file)");
+
+		state_t *s;
+		s = hash_get(&db.states, "host01/cpu");
+		isnt_null(s, "state host01/cpu created");
+		is_pointer(s->type,
+			hash_get(&db.types, ":test"),  "host01/cpu is a :local check");
+		is_int(s->last_seen, 0,             "host01/cpu has not yet been seen");
+		ok(abs(now + 300 - s->expiry) < 5,  "host01/cpu expires within the next 5min");
+		is_int(s->status, PENDING,          "host01/cpu status is PENDING");
+		ok(!s->stale,                       "host01/cpu is not (yet) stale");
+
+		s = hash_get(&db.states, "site/online");
+		isnt_null(s, "state site/online created");
+		is_pointer(s->type,
+			hash_get(&db.types, ":cloud"),  "site/online is a :cloud check");
+		is_int(s->last_seen, 0,             "site/online has not yet been seen");
+		ok(abs(now + 900 - s->expiry) < 5,  "site/online expires within the next 15min");
+		is_int(s->status, PENDING,          "site/online status is PENDING");
+		ok(!s->stale,                       "site/online is not (yet) stale");
+	}
+
+	subtest { /* various configuration errors */
+		db_t     db;  memset(&db,  0, sizeof(db));
+		server_t svr; memset(&svr, 0, sizeof(svr));
+
+		write_file("t/tmp/config.bad", "listener :type\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with bad listener directive");
+
+		write_file("t/tmp/config.bad", "whatever works\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with unknown directive");
+
+		write_file("t/tmp/config.bad", "state\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with bad state directive");
+
+		write_file("t/tmp/config.bad", "literal\1\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with non-alpha directive");
+
+		write_file("t/tmp/config.bad", "use :::lots:of:colons\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with malformed type name");
+
+		write_file("t/tmp/config.bad", "type :x { foobar }\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with bad type definition");
+
+		write_file("t/tmp/config.bad", "type :x { { { {\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with REALLY bad type definition");
+
+		write_file("t/tmp/config.bad", "state :enoent x\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with missing type");
+
+		write_file("t/tmp/config.bad", "use :enoent\nstate x\n", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) != 0,
+			"should fail to read configuration file with missing (default) type");
+	}
+
+	subtest { /* recoverable configuration errors */
+		db_t     db;  memset(&db,  0, sizeof(db));
+		server_t svr; memset(&svr, 0, sizeof(svr));
+
+		write_file("t/tmp/config.bad",
+			"type :string {\n"
+			"  freshness 60\n"
+			"  critical \"this is an unterminated string literal\n"
+			"}\n"
+			"type :default { }\n"
+			"type :automsg { freshness 60 }\n"
+			"type :qhour   { freshness 900 }\n"
+			"type :hourly  { freshness 3600 }\n"
+			"type :hourly5 { freshness 3900 }\n"
+			"type :short   { freshness 42 }\n"
+			"", 0);
+		ok(configure("t/tmp/config.bad", &svr, &db) == 0,
+			"can recover from unterminated string");
+
+		type_t *t;
+		t = hash_get(&db.types, ":string");
+		isnt_null(t, "type :string created");
+		is_int(t->freshness, 60, ":string has a freshness threshold");
+		is_int(t->status, CRITICAL, ":string type has a staleness status");
+		is(t->summary, "this is an unterminated string literal", ":string type has a staleness summary");
+
+		t = hash_get(&db.types, ":default");
+		isnt_null(t, "type :default created");
+		is_int(t->freshness, 300, ":default has a default freshness threshold");
+		is_int(t->status, WARNING, ":default type has a default staleness status of WARNING");
+		is(t->summary, "No results received for more than 5 minutes", ":default type has a default staleness summary");
+
+		t = hash_get(&db.types, ":automsg");
+		isnt_null(t, "type :automsg created");
+		is_int(t->status, WARNING, ":automsg type has a default staleness status of WARNING");
+		is(t->summary, "No results received for more than 1 minute", ":automsg type has a default staleness summary");
+
+		t = hash_get(&db.types, ":qhour");
+		isnt_null(t, "type :qhour created");
+		is(t->summary, "No results received for more than 15 minutes", ":qhour type has a default staleness summary");
+
+		t = hash_get(&db.types, ":hourly");
+		isnt_null(t, "type :hourly created");
+		is(t->summary, "No results received for more than 1 hour", ":hourly type has a default staleness summary");
+
+		t = hash_get(&db.types, ":hourly5");
+		isnt_null(t, "type :hourly5 created");
+		is(t->summary, "No results received for more than 1 hour", ":hourly5 type has a default staleness summary");
+
+		t = hash_get(&db.types, ":short");
+		isnt_null(t, "type :short created");
+		is(t->summary, "No results received for more than 42 seconds", ":short type has a default staleness summary");
 	}
 }
