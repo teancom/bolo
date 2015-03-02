532065afb661dfe743b186e9f6d595b09a0fe9e5
James Hunt <james@niftylogic.com>
2015-03-01 22:03:35 -0500

    Update LICENSE

--- t/10-match.c
+++ t/10-match.c
@@ -1,3 +1,22 @@
+/*
+  Copyright 2015 James Hunt <james@jameshunt.us>
+
+  This file is part of Bolo.
+
+  Bolo is free software: you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+
+  Bolo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  You should have received a copy of the GNU General Public License along
+  with Bolo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "test.h"
 
 #define TEST_CONFIG_FILE "t/tmp/bolo.cfg"
