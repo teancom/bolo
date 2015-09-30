9500fbae21405152d04004f805b9436fdd1155cc
James Hunt <james@niftylogic.com>
2015-09-30 03:44:40 +0000

    Remove dead vx_* functions

--- src/util.c
+++ src/util.c
@@ -19,28 +19,6 @@
 
 #include "bolo.h"
 
-pdu_t *vx_pdu_dup(pdu_t *from, const char *type)
-{
-	pdu_t *to = pdu_make(type ? type : pdu_type(from), 0);
-	if (vx_pdu_copy(to, from, 1, 0) != 0) {
-		pdu_free(to);
-		return NULL;
-	}
-	return to;
-}
-
-int vx_pdu_copy(pdu_t *to, pdu_t *from, int start, int n)
-{
-	int i, end = n ? start + n : pdu_size(from);
-	for (i = start; i < pdu_size(from) && i < end; i++) {
-		size_t len;
-		uint8_t *buf = pdu_segment(from, i, &len);
-		pdu_extend(to, buf, len);
-		free(buf);
-	}
-	return 0;
-}
-
 int vx_vzmq_connect(void *z, const char *endpoint)
 {
 	strings_t *names = vzmq_resolve(endpoint, AF_UNSPEC);
